47abee5f7f170963a201cc33dfa2a0eaf34c21f5
Dhruvidave1 <dhruvi@ualberta.ca>
2023-01-08 16:49:13 -0700

    added skeleton home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-
-// Arrow function 
+// Arrow function
 const App = () => {
   return (
     <>
-    <h1> Welcome to Glow Recipe! ✨ </h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1> Fruit-Forward 🍉. Clinically Effective 🧪. Glowing Skin ✨ </h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
