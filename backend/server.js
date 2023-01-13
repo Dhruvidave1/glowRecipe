741e37338ef9c0aebc8670f4aa58dd3a672d5560
Dhruvidave1 <dhruvi@ualberta.ca>
2023-01-13 15:26:50 -0700

    initialized express

---
+++ backend/server.js
@@ -0,0 +1,4 @@
+//This syntax is common JS, in front end we are using import syntax which is es modules
+const express = require('express')
+const app = express()
+app.listen(5000, console.log('server running on port 5000'))
