935bca9c0b7d8ad19dd25f12cdc5899f34f2c37d
Dhruvidave1 <dhruvi@ualberta.ca>
2023-01-13 20:54:47 -0700

    backend work

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,12 +2,23 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ProductScreen = () => {
   const params = useParams()
-  const product = products.find((p) => p._id === params.id)
-  console.log(product.countInStock)
+  const [product, setProduct] = useState({})
+
+  //as soon as the page loads this gets called
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`)
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [])
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
